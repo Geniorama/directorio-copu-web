b3450754ec9f59c9c165c0f7416192f5c03d75eb
angelpublicista <angelpublicista.1@gmail.com>
2024-07-09 21:49:43 -0500

    fix: data sectors

---
+++ src/app/data/sectors.ts
@@ -0,0 +1,134 @@
+import type { Sector } from "../types"
+
+export const sectors:Sector[] = [
+    {
+        id: "1",
+        name: "Agencias de publicidad",
+        slug: "agencias-de-publicidad"
+       },
+       {
+        id: "2",
+        name: "Agencias BTL",
+        slug: "agencias-btl"
+       },
+       {
+        id: "3",
+        name: "Consultoras",
+        slug: "consultoras"
+       },
+       {
+        id: "4",
+        name: "Agencia de Relaciones Públicas",
+        slug: "agencia-de-relaciones-publicas"
+       },
+       {
+        id: "5",
+        name: "Agencias de Influencer Marketing",
+        slug: "agencias-de-influencer-marketging"
+       },
+       {
+        id: "6",
+        name: "Agencias y centrales de medios nuevas o independientes",
+        slug: "agencias-y-centrales-de-medios-nuevas-o-independientes"
+       },
+       {
+        id: "7",
+        name: "Agencias de Marketing Digital con servicios extraordinarios",
+        slug: "agencias-de-marketing-digital-con-servicios-extraordinarios"
+       },
+       {
+        id: "8",
+        name: "Productoras Audiovisuales",
+        slug: "productoras-audiovisuales"
+       },
+       {
+        id: "9",
+        name: "Post Productoras Audiovisuales",
+        slug: "post-productoras-audiovisuales"
+       },
+       {
+        id: "10",
+        name: "Fotógrafos y Estudios de Fotografía",
+        slug: "fotografos-y-estudios-de-fotografia"
+       },
+       {
+        id: "11",
+        name: "Realizadores y/o creadores de contenido digital",
+        slug: "realizadores-yo-creadores-de-contenido-digital"
+       },
+       {
+        id: "12",
+        name: "Estudios de VR, AR, experiencias inmersivas y CGI",
+        slug: "estudios-de-vr-ar-experiencias-inmersivas-y-cgi"
+       },
+       {
+        id: "13",
+        name: "Estudios de audio",
+        slug: "estudios-de-audio"
+       },
+       {
+        id: "14",
+        name: "Estudios de diseño, branding, y de packaging",
+        slug: "estudios-de-diseno-branding-y-de-packaging"
+       },
+       {
+        id: "15",
+        name: "Empresas de desarrollo de aplicaciones móviles",
+        slug: "empresas-de-desarrollo-de-aplicaciones-moviles"
+       },
+       {
+        id: "16",
+        name: "Empresas de servicios de impresión innovadores",
+        slug: "empresas-de-servicios-de-impresion-innovadores"
+       },
+       {
+        id: "17",
+        name: "Empresas de servicios tecnológicos innovadores",
+        slug: "empresas-de-servicios-tecnologicos-innovadores"
+       },
+       {
+        id: "18",
+        name: "Empresas de E-commerce y Soluciones de Comercio Electrónico",
+        slug: "empresas-de-e-commerce-y-soluciones-de-comercio-electronico"
+       },
+       {
+        id: "19",
+        name: "Empresas de análisis de datos",
+        slug: "empresas-de-analisis-de-datos"
+       },
+       {
+        id: "20",
+        name: "Empresas de Investigación/análisis de Mercados, audiencias",
+        slug: "empresas-de-investigacionanalisis-de-mercados-audiencias"
+       },
+       {
+        id: "21",
+        name: "Empresas de Publicidad exterior (OOH), pantallas",
+        slug: "empresas-de-publicidad-exterior-ooh-pantallas"
+       },
+       {
+        id: "22",
+        name: "Empresas de diseño y desarrollo web",
+        slug: "empresas-de-diseno-y-desarrollo-web"
+       },
+       {
+        id: "23",
+        name: "Empresas de UX y CX",
+        slug: "empresas-de-ux-y-cx"
+       },
+       {
+        id: "24",
+        name: "Servicios legales",
+        slug: "servicios-legales"
+       },
+       {
+        id: "25",
+        name: "Formación en Publicidad y Marketing",
+        slug: "formacion-en-publicidad-y-marketing"
+       },
+       {
+        id: "26",
+        name: "Bancos de imágenes",
+        slug: "bancos-de-imagenes"
+       }
+]
--- src/app/layout/SimpleLayout/Sidebar/WidgetSectores.tsx
+++ src/app/layout/SimpleLayout/Sidebar/WidgetSectores.tsx
@@ -1,48 +1,7 @@
 import { TitleWidget } from "@/app/utils/TitleWidget";
 import Icon from "../../../../../public/img/category.svg";
 import useFilter from "@/app/hooks/useFilter";
-
-const sectores = [
-  {
-    id: 1,
-    name: "Sector 1",
-  },
-
-  {
-    id: 2,
-    name: "Sector 2",
-  },
-
-  {
-    id: 3,
-    name: "Sector 3",
-  },
-
-  {
-    id: 4,
-    name: "Sector 3",
-  },
-
-  {
-    id: 5,
-    name: "Sector 3",
-  },
-
-  {
-    id: 6,
-    name: "Sector 3",
-  },
-
-  {
-    id: 7,
-    name: "Sector 3",
-  },
-
-  {
-    id: 8,
-    name: "Sector 3",
-  },
-];
+import { sectors } from "@/app/data/sectors";
 
 export default function WidgetSectores() {
 
@@ -53,9 +12,9 @@ export default function WidgetSectores() {
       <TitleWidget title="Sectores Creativos" icon={<img src={Icon.src} />} />
 
       <ul className="text-xs mt-4 max-h-[30vh] overflow-y-scroll custom-scroll">
-        <li onClick={addAllSectors} className={`py-[2px] cursor-pointer ${sector.includes('all') ? 'text-primary-color font-bold' : 'font-light'}`}>Todos los sectores creativos</li>
-        {sectores.map((item) => (
-          <li onClick={() => addSector(item.name)} className={`py-[2px] hover:font-bold cursor-pointer transition ${sector.includes(item.name) ? 'text-primary-color font-bold' : 'font-light'}`} key={item.id}>
+        <li onClick={addAllSectors} className={`py-1 cursor-pointer ${sector.includes('all') ? 'text-primary-color font-bold' : 'font-light'}`}>Todos los sectores creativos</li>
+        {sectors.map((item) => (
+          <li onClick={() => addSector(item.name)} className={`py-1 hover:font-bold cursor-pointer transition ${sector.includes(item.name) ? 'text-primary-color font-bold' : 'font-light'}`} key={item.id}>
             {item.name}
           </li>
         ))}
