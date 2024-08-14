c8cc6aeeba8f3b340d17583183dfe4015e51d34d
angelpublicista <angelpublicista.1@gmail.com>
2024-08-14 13:23:13 -0500

    fix: cache no store in company page

--- src/app/empresas/[slug]/page.tsx
+++ src/app/empresas/[slug]/page.tsx
@@ -1,33 +1,30 @@
-import SingleCompanyPage from "@/app/views/SingleCompanyPage/SingleCompanyPage"
-import { getClient } from "@/lib/apollo-client"
-import { GetCompanyBySlug } from "@/app/api/queries"
+import SingleCompanyPage from "@/app/views/SingleCompanyPage/SingleCompanyPage";
+import { getClient } from "@/lib/apollo-client";
+import { GetCompanyBySlug } from "@/app/api/queries";
 
 type CompanyProps = {
-  params: {slug:string}
-}
+  params: { slug: string };
+};
 
 const contexDefault = {
-  fetchOptions:{
-    next: { revalidate: 5 }
-  }
-}
+  fetchOptions: {
+    // next: { revalidate: 5 },
+    cache: 'no-store'
+  },
+};
 
 async function loadData(slug: string) {
-   const res = await getClient().query({
+  const res = await getClient().query({
     query: GetCompanyBySlug,
     variables: { slug },
-    fetchPolicy: 'no-cache',
-    context: contexDefault
-   })
+    fetchPolicy: "no-cache",
+    context: contexDefault,
+  });
 
-   return res.data.companies.data[0]
-} 
+  return res.data.companies.data[0];
+}
 
-export default async function Company({params}:CompanyProps) {
-  const data = await loadData(params.slug)
-  return (
-    <SingleCompanyPage
-      data={data}
-    />
-  )
+export default async function Company({ params }: CompanyProps) {
+  const data = await loadData(params.slug);
+  return <SingleCompanyPage data={data} />;
 }
