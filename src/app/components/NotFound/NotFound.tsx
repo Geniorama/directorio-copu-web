ae710c8c6b5f357e079b2a327ff3c9e320a32992
angelpublicista <angelpublicista.1@gmail.com>
2024-08-16 15:48:21 -0500

    fix: input search for one character

--- src/app/components/NotFound/NotFound.tsx
+++ src/app/components/NotFound/NotFound.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useAppSelector } from "@/lib/hooks/hooks";
+import { useAppSelector, useAppDispatch } from "@/lib/hooks/hooks";
+import { filterSearch } from "@/lib/features/searchSlice";
 
 export default function NotFound() {
   const allSectors = useAppSelector((state) => state.searchReducer.allSectors);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="h-[60vh] flex flex-col gap-4 justify-center items-center p-5 text-center">
@@ -12,11 +14,11 @@ export default function NotFound() {
         probar con alguno de estos temas?
       </p>
 
-      <ul className="flex gap-12">
+      <ul className="flex gap-12 max-w-[900px] flex-wrap text-center justify-center">
         {allSectors &&
           allSectors.length > 0 &&
           allSectors.map((sector) => (
-            <li key={sector.slug}>
+            <li onClick={() => dispatch(filterSearch(sector.name))} key={sector.slug}>
               <span className="text-lg lg:text-2xl underline font-light cursor-pointer hover:opacity-70 transition">{sector.name}</span>
             </li>
           ))}
--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -5,7 +5,7 @@ import IconSearch from "../../../../public/img/search.svg";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks/hooks";
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import { filterSearch } from "@/lib/features/searchSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type SearchBarProps = {
   placeholder?: string;
@@ -14,26 +14,16 @@ type SearchBarProps = {
 
 export default function SearchBar({placeholder, backgroundColor}: SearchBarProps) {
   const searchValue = useAppSelector((state) => state.searchReducer.value);
+  const [searchInputValue, setSearchInputValue] = useState(searchValue)
+
   const dispatch = useAppDispatch();
-  const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
-    if (pathname === "/" && inputValue.length >= 3) {
-      dispatch(filterSearch(inputValue));
-    } else {
-      dispatch(filterSearch(''))
-    }
-  };
-
-  const handleKeyDown = (e:React.KeyboardEvent<HTMLInputElement>) => {
-    if (pathname !== "/" && e.key === 'Enter') {
-      e.preventDefault();
-      const inputValue = (e.target as HTMLInputElement).value;
-      router.push(`/?s=${inputValue}`);
-    }
+    setSearchInputValue(inputValue)
+    dispatch(filterSearch(inputValue));
   };
 
   useEffect(() => {
@@ -45,16 +35,19 @@ export default function SearchBar({placeholder, backgroundColor}: SearchBarProps
     }
   }, [dispatch, pathname, searchParams])
 
+  useEffect(() => {
+    setSearchInputValue(searchValue)
+  },[searchValue])
+
   return (
     <div className={`flex items-center gap-3 ${backgroundColor ? backgroundColor : 'bg-secondary-color'} p-3 rounded-full`}>
       <img src={IconSearch.src} alt="" />
       <input
-        defaultValue={searchValue}
         onChange={(e) => handleChange(e)}
-        onKeyDown={(e) => handleKeyDown(e)}
         className="bg-transparent w-full outline-none text-lg"
         type="search"
         placeholder={placeholder}
+        value={searchInputValue}
       />
     </div>
   );
