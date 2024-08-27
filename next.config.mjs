c7da43932be46da6dfc67bbec1e5b1898ba3fae5
angelpublicista <angelpublicista.1@gmail.com>
2024-08-27 09:07:33 -0500

    fix: update redirections

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/tyc',
+          destination: 'https://s3.copu.media/TERMINOS%20Y%20CONDICIONES%20DIRECTORIO%20BY%20COPU.pdf', // Reemplaza con la URL a la que deseas redirigir.
+          permanent: true, // true si deseas que sea una redirección 301 (permanente).
+        },
 
-export default nextConfig;
+        {
+            source: '/habeasdata',
+            destination: 'https://s3.copu.media/AUTORIZACION%20PARA%20EL%20TRATAMIENTO%20DE%20DATOS%20DIRECTORIO%20COPU.pdf', // Reemplaza con la URL a la que deseas redirigir.
+            permanent: true, // true si deseas que sea una redirección 301 (permanente).
+          },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
