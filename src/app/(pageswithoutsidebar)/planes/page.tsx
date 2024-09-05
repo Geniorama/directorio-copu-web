7500cfd7572bf3b6a5b9c0ec11612aee87a09289
angelpublicista <angelpublicista.1@gmail.com>
2024-09-05 09:44:09 -0500

    feat: seo title in pages

--- src/app/(pageswithoutsidebar)/planes/page.tsx
+++ src/app/(pageswithoutsidebar)/planes/page.tsx
@@ -1,11 +1,17 @@
 import { PlansPage } from "@/app/views/PlansPage"
 import { getClient } from "@/lib/apollo-client"
 import { GetPlans } from "@/app/api/queries"
+import { Metadata } from "next"
 
 async function loadData() {
   const {data} = await getClient().query(GetPlans)
   return data.plans.data
 }
+
+export const metadata: Metadata = {
+  title: "Planes Directorio | Directorio Copu",
+  description: "Conoce los planes de suscripción",
+};
 
 export default async function Plans() {
   const plans = await loadData()
--- src/app/(pageswithoutsidebar)/que-es/page.tsx
+++ src/app/(pageswithoutsidebar)/que-es/page.tsx
@@ -1,4 +1,10 @@
-import { WhatIsPage } from "@/app/views/WhatIsPage"
+import { WhatIsPage } from "@/app/views/WhatIsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Qué es el directorio | Directorio Copu",
+  description: "Conoce más sobre copu ny el directorio de empresas.",
+};
 
 export default function WhatIs() {
   return (
